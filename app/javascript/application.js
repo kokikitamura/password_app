e12f18c1bb6d9064f767cdeea680da6c28a2bf8c
kokikitamura <ktmrkok1130@gmail.com>
2024-09-08 15:42:39 +0900

    レスポンシブとハンバーガーメニュー

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,42 +4,11 @@ import "controllers"
 import jquery from "jquery"
 window.$ = jquery
 
-// $(function() {
-//   $('#js-copybtn').on('click', function(){
-//     // コピーする文章の取得
-//     let text = $('#js-copytext').text();
-//     // テキストエリアの作成
-//     let $textarea = $('<textarea></textarea>');
-//     // テキストエリアに文章を挿入
-//     $textarea.text(text);
-//     // テキストエリアを挿入
-//     $(this).append($textarea);
-//     // テキストエリアを選択
-//     $textarea.select();
-//     // コピー
-//     document.execCommand('copy');
-//     // テキストエリアの削除
-//     $textarea.remove();
-//     alert('「'+text+'」をコピーしました');
-//   });
-// });
 
-// $(function() {
-// 	$('.copy_btn').on('click', function(){
-// 		let text = $('.copy_txt').text();
-// 		let textarea = $('<textarea></textarea>');
-// 		textarea.text(text);
-// 		$(this).append(textarea);
-// 		textarea.select();
-// 		document.execCommand('copy');
-// 		textarea.remove();
-// 		alert('「'+text+'」をコピーしました');
-// 	});
-// });
+document.addEventListener("turbo:load", setupPasswordGenerator);
+document.addEventListener("turbo:render", setupPasswordGenerator);
 
-
-//パスワードジェネレーター
-document.addEventListener("turbo:load", function() {
+function setupPasswordGenerator() {
   function generatePassword(length, includeCapital, includeNumbers, includeSymbols) {
     let charset = "abcdefghijklmnopqrstuvwxyz";
     if (includeCapital) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -53,7 +22,7 @@ document.addEventListener("turbo:load", function() {
     return password;
   }
 
-  $("#generate_password_button").click(function() {
+  $("#generate_password_button").off('click').on('click', function() {
     // 各オプションの値を取得
     const length = $("#password_length").val();
     let includeCapital = $("#include_capital").is(":checked");
@@ -66,38 +35,8 @@ document.addEventListener("turbo:load", function() {
     // パスワード入力フォームにセット
     $("#password_field").val(password);
   });
-});
+}
 
-
-
-document.addEventListener("turbo:render", function() {
-  function generatePassword(length, includeCapital, includeNumbers, includeSymbols) {
-    let charset = "abcdefghijklmnopqrstuvwxyz";
-    if (includeCapital) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (includeNumbers) charset += "0123456789";
-    if (includeSymbols) charset += "!@#$%";
-
-    let password = "";
-    for (let i = 0; i < length; i++) {
-      password += charset[Math.floor(Math.random() * charset.length)];
-    }
-    return password;
-  }
-
-  $("#generate_password_button").click(function() {
-    // 各オプションの値を取得
-    const length = $("#password_length").val();
-    let includeCapital = $("#include_capital").is(":checked");
-    let includeNumbers = $("#include_numbers").is(":checked");
-    let includeSymbols = $("#include_symbols").is(":checked");
-
-    // パスワード生成
-    const password = generatePassword(length, includeCapital, includeNumbers, includeSymbols);
-
-    // パスワード入力フォームにセット
-    $("#password_field").val(password);
-  });
-});
 
 //パスワードのコピーボタン
 $(document).on('click', '#pass-copy', function(){
@@ -124,3 +63,44 @@ $(document).on('click', '#account-copy', function(){
       navigator.clipboard.writeText(text);
   }
 });
+
+//ヘッダーのドロップダウン
+//$(document).on('turbolinks:load', function() {
+// $(function() {
+//   $(".nav").children("li").hover(function(){
+//     $(".dropdown-lists").stop().slideToggle(100);
+//   });
+// });
+// });
+// $(document).on('turbolinks:load', function() {
+//   $(function () {
+//     $('#js-hamburger-menu, .hamburger-link').on('click', function () {
+//       $('.hamburger-nav').slideToggle(200)
+//       $('.hamburger-menu').toggleClass('hamburger-menu--open')
+//     });
+//   });
+// });
+
+// document.addEventListener("turbo:load", function() {
+//   $('#js-hamburger-menu, .hamburger-link').on('click', function () {
+//     $('.hamburger-nav').slideToggle(200)
+//     $('.hamburger-menu').toggleClass('hamburger-menu--open')
+//   });
+// });
+
+// document.addEventListener("turbo:render", function() {
+//   $('#js-hamburger-menu, .hamburger-link').on('click', function () {
+//     $('.hamburger-nav').slideToggle(200)
+//     $('.hamburger-menu').toggleClass('hamburger-menu--open')
+//   });
+// });
+
+document.addEventListener("turbo:load", setupHamburgerMenu);
+document.addEventListener("turbo:render", setupHamburgerMenu);
+
+function setupHamburgerMenu() {
+  $('#hamburger-menu, .hamburger-link').off('click').on('click', function () {
+    $('.hamburger-nav').slideToggle(200);
+    $('.hamburger-menu').toggleClass('hamburger-menu--open');
+  });
+}
